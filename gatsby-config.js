86cf2d803a09675631f06b3cc62dd177c631c0a1
Katherine Hu <katherine@katherines-air.lan>
2019-07-31 13:18:31 -0400

    watch button text 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Katherine Hu',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Coder, high schooler, and poodle ethusiast.</h1>
+    <p>Loves to code, junior in high school and hack mason club lead.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Go to page 2!!!</Link>
   </div>
 )
 
